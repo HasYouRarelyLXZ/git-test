eef0aa352846134270866b9bd73e62d686a844e4
HasYouRarelyLXZ <15710328717@163.com>
2018-06-08 20:16:57 +0800

    git分支测试1.0

---
+++ src/main/java/com/baizhi/entity/Person.java
@@ -0,0 +1,52 @@
+package com.baizhi.entity;
+
+import java.io.Serializable;
+
+public class Person implements Serializable{
+    private  String name;
+    private  String password;
+    private  String sex;
+    private  String year;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", password='" + password + '\'' +
+                ", sex='" + sex + '\'' +
+                ", year='" + year + '\'' +
+                '}';
+    }
+}
